45b5293099db7f5f06b05733bcf4a6b194c38c71
tomarv2 <varuntomar2019@gmail.com>
2022-02-03 17:59:59 -0800

    initial commit

--- examples/azure_databricks_with_virtual_network/main.tf
+++ examples/azure_databricks_with_virtual_network/main.tf
@@ -1,5 +1,5 @@
 module "vnet" {
-  source = "git@github.com:tomarv2/terraform-azure-virtual-network.git?ref=v0.0.2"
+  source = "git::git@github.com:tomarv2/terraform-azure-virtual-network.git?ref=v0.0.2"
 
   resource_group_name = "demo-resource_group"
   location            = "westus2"
